95e920f6fe2de0b26cd8af072b29438c9a8e83cc
Anton Bobov <abobov@gmail.com>
2017-09-14 22:51:28 +0500

    Add battery status command.

---
+++ battery-status.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Print current battery status.  Can be used in genmon xfce plugin.
+
+print_sepparator=false
+
+print_status() {
+    [ $print_sepparator = true ] && printf ' '
+
+    case "$status" in
+        "Full")
+            return;;
+        "Discharging")
+            status="-";;
+        "Charging")
+            status="+";;
+        "Unknown")
+            # Probably rich the threshold
+            if [ $(echo "$current_charge > 50" | bc -l) == 1 ] ; then
+                return
+            fi
+            status="?"
+            ;;
+        *)
+            status="?";;
+    esac
+
+    printf '%s %.0f%s' $(basename $bat)  $current_charge "$status"
+
+    print_sepparator=true
+}
+
+for bat in /sys/class/power_supply/BAT? ; do
+    status=$(cat "$bat/status")
+    energy_full=$(cat "$bat/energy_full")
+    energy_now=$(cat "$bat/energy_now")
+    current_charge=$(bc <<< "scale=2; $energy_now / $energy_full * 100")
+
+    print_status
+done
+echo
