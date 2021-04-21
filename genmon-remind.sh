9b340bc745a39699b0ee66c2521b81d2499407ce
Anton Bobov <abobov@gmail.com>
2021-04-21 22:21:30 +0500

    Updates.

--- genmon-remind.sh
+++ genmon-remind.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-TODAY_REMINDERS=$(rem -r -f -q | grep -v '^>' | wc -l)
+TODAY_REMINDERS=$(rem -r -f -q | fgrep -x -v 'No reminders.' | grep -v '^>' | wc -l)
 CAL_IMG="/usr/share/icons/Adwaita/scalable/mimetypes/x-office-calendar-symbolic.svg"
 
 if [ -f "$CAL_IMG" ] ; then
     echo "<img>$CAL_IMG</img>"
 fi
-[ $TODAY_REMINDERS -gt 1 ] && echo "<txt> $TODAY_REMINDERS</txt>"
+[ $TODAY_REMINDERS -gt 0 ] && echo "<txt> $TODAY_REMINDERS</txt>"
 echo "<click>tkremind -m -b1 </click>"
 echo "<tool>"
 echo "<span font_family='monospace' size='small'>"
--- sync-rem.sh
+++ sync-rem.sh
@@ -3,7 +3,8 @@
 set -e
 
 PREV_MONTHS=2
-MONTHS=$((12 + PREV_MONTHS))
+MONTHS=$((14 + PREV_MONTHS))
 START=$(date --date "$PREV_MONTHS months ago" +%F)
 chronic gcalcli --calendar remind delete --iamaexpert '*'
-rem -ppp$MONTHS -b1 -m $START | rem2ics | chronic gcalcli --calendar remind import -
+#rem -ppp$MONTHS -b1 -m $START | rem2ics | chronic gcalcli --calendar remind import -
+rem -ppp$MONTHS -b1 -m $START | rem2ics > remind.ics
